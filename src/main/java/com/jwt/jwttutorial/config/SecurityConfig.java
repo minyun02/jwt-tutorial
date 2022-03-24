8ef7013785d8ae375b836a77833868a58b6dec6b
min <www5981@naver.com>
2022-03-24 20:05:00 +0900

    configure 등록

--- src/main/java/com/jwt/jwttutorial/config/SecurityConfig.java
+++ src/main/java/com/jwt/jwttutorial/config/SecurityConfig.java
@@ -1,34 +1,96 @@
 package com.jwt.jwttutorial.config;
 
+import com.jwt.jwttutorial.jwt.JwtAccessDeniedHandler;
+import com.jwt.jwttutorial.jwt.JwtAuthenticationEntryPoint;
+import com.jwt.jwttutorial.jwt.JwtSecurityConfig;
+import com.jwt.jwttutorial.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
-@EnableWebSecurity //웹 보안을 활성화 하겠다
+@EnableWebSecurity//웹 보안을 활성화 하겠다
 /*
 추가적인 설정으로
 1. WebSecurityConfigurer를 implements
 2. WebSecurityConfigurerAdapter를 extends
  */
+@EnableGlobalMethodSecurity(prePostEnabled = true)//@PreAuthroize 어노테이션을 메소드단위로 추가하기위해서 적용
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final TokenProvider tokenProvider;
+    private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-                .ignoring()
-                .antMatchers("/h2-console/**"
-                        ,"/favicon.ico");
-
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+            CorsFilter corsFilter,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.corsFilter = corsFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
     }
 
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests() //httpServletRequest를 사용하는 요청들에 접근제한을 설정하겠다
-                .antMatchers("/api/hello").permitAll() //api/hello에 대한 접근은 인증없이 허용하겠다.
-                .anyRequest().authenticated();//나머지 요청들은 모두 인증을 받아야한다.
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        ,"/favicon.ico"
+                        ,"/error"
+                );
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf().disable()
+
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // enable h2-console
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
+                /*
+                로그인 api, 회원가입 apir는 토큰이 없는 상태에서 요청이 들어오기 때문에 모두 열어준다.
+                 */
+
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+    }
 }
