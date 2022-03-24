7759f4238d304f203f402143f67417ab83c72b09
min <www5981@naver.com>
2022-03-24 20:07:32 +0900

    jwt

---
+++ src/main/java/com/jwt/jwttutorial/jwt/JwtSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.jwt.jwttutorial.jwt;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+/*
+tokenProvider를 주입받고 jwtFilter를 통해 security로직에 필터를 등록한다.
+ */
+    private final TokenProvider tokenProvider;
+
+    public JwtSecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) {
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
